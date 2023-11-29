abe6966e7099e84ab6d6cd1f357d219f056b3d9d
Daniel Chiquete <115588244+danielcn18@users.noreply.github.com>
2023-11-29 13:57:59 -0700

    Update index.js

--- src/util/index.js
+++ src/util/index.js
@@ -1,2 +1,2 @@
-export const baseURL = `http://localhost:4000`
-// export const baseURL = "http://mern-chat-app-b96k.onrender.com"
+// export const baseURL = `http://localhost:4000`
+export const baseURL = "https://chat-app-server-f9xz.onrender.com"
